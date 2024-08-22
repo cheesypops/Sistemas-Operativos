b4ac25e23fb5b5501200a4c220144bc68d0996c8
cheesypops <00030821@uca.edu.sv>
2024-08-21 22:25:13 -0600

    Add: labo1

---
+++ SO/Labo1/Counter.cs
@@ -0,0 +1,94 @@
+﻿namespace Labo1;
+
+public class Counter
+{
+    // atributos
+    private int numCount; // contador de numeros
+    private char minusCount; // contador de minusculas
+    private char capitalCount; //contador de mayusculas
+    
+    private Boolean stopMinus = false; // bandera para detener el contador de minusculas
+    private Boolean stopNum = false; // bandera para detener el contador de numeros
+    
+    // constructor
+    public Counter()
+    {
+        numCount = 0;
+        minusCount = 'a';
+        capitalCount = 'A';
+    }
+    
+    // metodos
+    // getters y setters---------------------------------
+    public int getNumCount()
+    {
+        return numCount;
+    }
+    
+    public char getMinusCount()
+    {
+        return minusCount;
+    }
+    
+    public char getCapitalCount()
+    {
+        return capitalCount;
+    }
+    
+    public Boolean getStopMinus()
+    {
+        return stopMinus;
+    }
+    
+    public Boolean getStopNum()
+    {
+        return stopNum;
+    }
+    // -----------------------------------------------------------------------------
+    public void setNumCount(int numCount)
+    {
+        this.numCount = numCount;
+    }
+    
+    public void setMinusCount(char minusCount)
+    {
+        this.minusCount = minusCount;
+    }
+    
+    public void setCapitalCount(char capitalCount)
+    {
+        this.capitalCount = capitalCount;
+    }
+    // ----------------------------------------------------------------------------------
+    public void incrementNumCount()
+    {
+        numCount++;
+        evaluateNumCount(); // evalua el contador de numeros
+        Thread.Sleep(100);
+    }
+    
+    public void incrementMinusCount()
+    {
+        minusCount++;
+        Thread.Sleep(200);
+    }
+    
+    public void incrementCapitalCount()
+    {
+        capitalCount++;
+        Thread.Sleep(150);
+    }
+    
+    // metodo para evaluar el contador de numeros--------------------------------------
+    public void evaluateNumCount()
+    {
+        if(numCount == 20)
+        {
+            // si el numero es igual a 20, detiene el contador de minusculas
+            stopMinus = true;
+            stopNum = true;
+            
+            Console.WriteLine("Se ha llegado al limite de numeros");
+        }
+    }
+}
---
+++ SO/Labo1/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Labo1;
+using System;
+using System.Threading;
+
+Console.WriteLine("Hello, World!");
+
+// instancia del contador
+Counter counter = new Counter();
+
+// instanciar los hilos junto con las funciones que van a desempeñar
+Thread numThread = new Thread(() =>
+{
+    while (!counter.getStopNum())
+    {
+        counter.incrementNumCount();
+        Console.WriteLine("Num: " + counter.getNumCount());
+    }
+});
+Thread minusThread = new Thread(() =>
+{
+    while (!counter.getStopNum())
+    {
+        counter.incrementMinusCount();
+        Console.WriteLine("Minus: " + counter.getMinusCount());
+    }
+});
+Thread capitalThread = new Thread(() =>
+{
+    while (counter.getCapitalCount() != 'Z')
+    {
+        counter.incrementCapitalCount();
+        Console.WriteLine("Capital: " + counter.getCapitalCount());
+    }
+});
+
+// iniciar los hilos, junto con sus funciones
+numThread.Start();
+minusThread.Start();
+capitalThread.Start();
+
+
+// esperar a que los hilos terminen
+numThread.Join();
+minusThread.Join();
+capitalThread.Join();
+
+Console.WriteLine("Fin del programa");
